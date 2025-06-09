c9d850b97973c2b0575f21c2603f674cd3b6db95
0marKh1i1 <okkhalil23@cit.just.edu.jo>
2025-06-09 21:06:50 +0300

    added individual page for images

---
+++ _next/static/chunks/app/gallery/[slug]/page-ab3ff8e92a6675fb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[168],{576:function(n,e,u){Promise.resolve().then(u.bind(u,2092))}},function(n){n.O(0,[809,789,665,240,516,464,7,971,117,744],function(){return n(n.s=576)}),_N_E=n.O()}]);
